49044ce7f75857f277cb0639df0fcc27267f4ad8
xjw415614 <2237154863@qq.com>
2018-06-26 01:52:05 +0000

    Create categories

---
+++ database/migrations/2018_06_26_094814_seed_categories_data.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        => '分享',
+                'description' => '分享创造，分享发现',
+            ],
+            [
+                'name'        => '教程',
+                'description' => '开发技巧、推荐扩展包等',
+            ],
+            [
+                'name'        => '问答',
+                'description' => '请保持友善，互帮互助',
+            ],
+            [
+                'name'        => '公告',
+                'description' => '站点公告',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+
+        //up() 方法中使用 DB 类的 insert() 批量往数据表 catetories 里插入数据 $catetories
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+    //down() 在回滚迁移时会被调用，是 up() 方法的逆反操作。truncate() 方法为清空 catetories 数据表里的所有数据。
+}
